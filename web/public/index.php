7d987bddd03b1323f6a3a6cce80af17098c59b95
Devboot <giluanjsouza@gmail.com>
2023-04-28 09:34:32 -0300

    Atualizar o index.php
    remoção de código comentado

--- web/public/index.php
+++ web/public/index.php
@@ -17,28 +17,12 @@ $foo = new App\Acme\Foo();
         </style>
     </head>
     <body>
-
-        <p>
-            A 10,000-point demo of <a href="https://github.com/Leaflet/Leaflet.heat">Leaflet.heat</a>, a tiny and fast Leaflet heatmap plugin.
-            <iframe class="ghbtns" src="http://ghbtns.com/github-btn.html?user=Leaflet&amp;repo=Leaflet.heat&amp;type=watch&amp;count=true"
-        allowtransparency="true" frameborder="0" scrolling="0" width="90" height="20"></iframe>
-        </p>
-
         <div id="map"></div>
-
-        <!-- <script src="../node_modules/simpleheat/simpleheat.js"></script>
-        <script src="../src/HeatLayer.js"></script> -->
         <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
         <script src="../js/leaflet-heat.js"></script>
 
         <script src="js/realworld.10000.js"></script>
         <script>
-
-            const res = await fetch("dados.json");
-            const data = await res.json();
-            console.log(data);
-            console.log(data);
-
             var map = L.map('map').setView([-37.87, 175.475], 12);
 
             var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
